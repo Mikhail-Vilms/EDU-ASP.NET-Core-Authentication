b28f3c5eb8a2d0d291cee881969a55df87853ae1
Mikhail-Vilms <mikhail.vilms@gmail.com>
2020-01-10 17:24:07 -0600

    WIP: Configuring JWT token authentication

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using EDU_ASP.NET_Core_Authentication.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace EDU_ASP.NET_Core_Authentication
 {
@@ -28,6 +32,37 @@ namespace EDU_ASP.NET_Core_Authentication
                 .AddEntityFrameworkStores<AuthenticationContext>();
 
             services.AddCors();
+
+            // JWT Authentication
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                // We can restrict authentication resourses only to type of https
+                x.RequireHttpsMetadata = false;
+                // Do we want to save authentication token inside the server after successful authentication?
+                x.SaveToken = false;
+
+                // Define how we want to validate token once it is recieved from the client side.
+
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    // System will validate the security key (???) during validation. (???)
+                    ValidateIssuerSigningKey = true,
+                    // String for the JWT encryption
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(Configuration["ApplicationSettings:JWT_Secret"].ToString())),
+                    // ???
+                    ValidateIssuer = false,
+                    // ???
+                    ValidateAudience = false,
+                    // Checking an expioration time of token(???)
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
